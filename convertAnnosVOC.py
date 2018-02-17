1ff3c1e3cbfbe764caa7a7aa78284ed0ee81f264
Emil Kattainen <emilkattainen@gmail.com>
2018-02-17 10:54:25 +0200

    Modify converter to support multiple objects add gitignore and modify used labels in config

--- convertAnnosVOC.py
+++ convertAnnosVOC.py
@@ -1,4 +1,5 @@
-from pgmagick import Image
+import os.path
+from PIL import Image
 import cv2
 
 
@@ -25,42 +26,52 @@ for line in content:
     folder = splitted_name[0]
     coords = splitted[1:5]
     fname = splitted_name[1][:-3]
-    #if folder == "00":
-    #    if splitted[5] not in names:
-    #        names+=[splitted[5]]
 
-    if folder != "00":
-        continue
-    try:
-        img = Image("images/" + fname + "jpeg")#continue
-        #img.write('images/' + fname + "jpeg")
-    except Exception:
-        import pdb; pdb.set_trace()
-        continue
+    img = Image.open("images/" + fname + "jpg")#continue
+    full_name = "annotations/" + fname + "xml"
 
-    with open("annotations/" + fname + "xml", 'w') as f:
-        f.write("<annotation verified=\"yes\">\n" +
-                "\t<folder>images</folder>\n" +
-                "\t<filename>" + fname + "jpeg</filename>\n" +
-                "\t<path>./images/" + fname + "</path>\n" +
-                "\t<source><database>BTSD</database></source>\n" +
-                "\t<size>\n" +
-                "\t\t<width>" + str(img.columns()) + "</width>\n" +
-                "\t\t<height>" + str(img.rows()) + "</height>\n" +
-                "\t\t<depth>" + str(3) + "</depth>\n" +
-                "\t</size>\n" +
-                "\t<segmented>0</segmented>\n" +
-                "\t<object>\n" +
-                "\t\t<name>" + splitted[5] + "</name>\n" +
-                "\t\t<pose>Unspecified</pose>\n" +
-                "\t\t<truncated>0</truncated>\n" +
-                "\t\t<difficult>0</difficult>\n" +
-                "\t\t<bndbox>\n" +
-                "\t\t\t<xmin>" + coords[0] + "</xmin>\n" +
-                "\t\t\t<ymin>" + coords[1] + "</ymin>\n" +
-                "\t\t\t<xmax>" + coords[2] + "</xmax>\n" +
-                "\t\t\t<ymax>" + coords[3] + "</ymax>\n" +
-                "\t\t</bndbox>\n" +
-                "\t</object>\n" +
-                "</annotation>")
+    if os.path.isfile(full_name):
+        with open(full_name, 'r') as f:
+            content = f.readlines()
+        content.insert(-1,
+                       "\t<object>\n" +
+                       "\t\t<name>" + splitted[5] + "</name>\n" +
+                       "\t\t<pose>Unspecified</pose>\n" +
+                       "\t\t<truncated>0</truncated>\n" +
+                       "\t\t<difficult>0</difficult>\n" +
+                       "\t\t<bndbox>\n" +
+                       "\t\t\t<xmin>" + coords[0] + "</xmin>\n" +
+                       "\t\t\t<ymin>" + coords[1] + "</ymin>\n" +
+                       "\t\t\t<xmax>" + coords[2] + "</xmax>\n" +
+                       "\t\t\t<ymax>" + coords[3] + "</ymax>\n" +
+                       "\t\t</bndbox>\n" +
+                       "\t</object>\n")
+        with open(full_name, 'w') as f:
+            f.write("".join(content))
+    else:
+        with open(full_name, 'w') as f:
+            f.write("<annotation verified=\"yes\">\n" +
+                    "\t<folder>images</folder>\n" +
+                    "\t<filename>" + fname + "jpg</filename>\n" +
+                    "\t<path>./images/" + fname + "</path>\n" +
+                    "\t<source><database>BTSD</database></source>\n" +
+                    "\t<size>\n" +
+                    "\t\t<width>" + str(img.size[0]) + "</width>\n" +
+                    "\t\t<height>" + str(img.size[1]) + "</height>\n" +
+                    "\t\t<depth>" + str(3) + "</depth>\n" +
+                    "\t</size>\n" +
+                    "\t<segmented>0</segmented>\n" +
+                    "\t<object>\n" +
+                    "\t\t<name>" + splitted[5] + "</name>\n" +
+                    "\t\t<pose>Unspecified</pose>\n" +
+                    "\t\t<truncated>0</truncated>\n" +
+                    "\t\t<difficult>0</difficult>\n" +
+                    "\t\t<bndbox>\n" +
+                    "\t\t\t<xmin>" + coords[0] + "</xmin>\n" +
+                    "\t\t\t<ymin>" + coords[1] + "</ymin>\n" +
+                    "\t\t\t<xmax>" + coords[2] + "</xmax>\n" +
+                    "\t\t\t<ymax>" + coords[3] + "</ymax>\n" +
+                    "\t\t</bndbox>\n" +
+                    "\t</object>\n" +
+                    "</annotation>")
 print names
