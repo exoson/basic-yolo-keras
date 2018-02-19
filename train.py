d4f799a5fbf47c578af4b88f4266e3c4cafc6137
Emil Kattainen <emilkattainen@gmail.com>
2018-02-19 10:59:59 +0200

    Modify config and stuff

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 """
-This script takes in a configuration file and produces the best model. 
+This script takes in a configuration file and produces the best model.
 The configuration file is a json file and looks like this:
 
 {
@@ -9,14 +9,14 @@ The configuration file is a json file and looks like this:
         "architecture":         "Full Yolo",
         "input_size":           416,
         "anchors":              [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828],
-        "max_box_per_image":    10,        
+        "max_box_per_image":    10,
         "labels":               ["raccoon"]
     },
 
     "train": {
         "train_image_folder":   "/home/andy/data/raccoon_dataset/images/",
-        "train_annot_folder":   "/home/andy/data/raccoon_dataset/anns/",      
-          
+        "train_annot_folder":   "/home/andy/data/raccoon_dataset/anns/",
+
         "train_times":          10,
         "pretrained_weights":   "",
         "batch_size":           16,
@@ -63,22 +63,22 @@ def _main_(args):
 
     config_path = args.conf
 
-    with open(config_path) as config_buffer:    
+    with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
 
     ###############################
-    #   Parse the annotations 
+    #   Parse the annotations
     ###############################
 
     # parse annotations of the training set
-    train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'], 
-                                                config['train']['train_image_folder'], 
+    train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'],
+                                                config['train']['train_image_folder'],
                                                 config['model']['labels'])
 
     # parse annotations of the validation set, if any, otherwise split the training set
     if os.path.exists(config['valid']['valid_annot_folder']):
-        valid_imgs, valid_labels = parse_annotation(config['valid']['valid_annot_folder'], 
-                                                    config['valid']['valid_image_folder'], 
+        valid_imgs, valid_labels = parse_annotation(config['valid']['valid_annot_folder'],
+                                                    config['valid']['valid_image_folder'],
                                                     config['model']['labels'])
     else:
         train_valid_split = int(0.8*len(train_imgs))
@@ -90,45 +90,45 @@ def _main_(args):
     if len(config['model']['labels']) > 0:
         overlap_labels = set(config['model']['labels']).intersection(set(train_labels.keys()))
 
-        print 'Seen labels:\t', train_labels
-        print 'Given labels:\t', config['model']['labels']
-        print 'Overlap labels:\t', overlap_labels           
+        print ('Seen labels:\t', train_labels)
+        print ('Given labels:\t', config['model']['labels'])
+        print ('Overlap labels:\t', overlap_labels)
 
         if len(overlap_labels) < len(config['model']['labels']):
-            print 'Some labels have no annotations! Please revise the list of labels in the config.json file!'
+            print ('Some labels have no annotations! Please revise the list of labels in the config.json file!')
             return
     else:
-        print 'No labels are provided. Train on all seen labels.'
+        print ('No labels are provided. Train on all seen labels.')
         config['model']['labels'] = train_labels.keys()
-        
+
     ###############################
-    #   Construct the model 
+    #   Construct the model
     ###############################
 
     yolo = YOLO(architecture        = config['model']['architecture'],
-                input_size          = config['model']['input_size'], 
-                labels              = config['model']['labels'], 
+                input_size          = config['model']['input_size'],
+                labels              = config['model']['labels'],
                 max_box_per_image   = config['model']['max_box_per_image'],
                 anchors             = config['model']['anchors'])
 
     ###############################
-    #   Load the pretrained weights (if any) 
-    ###############################    
+    #   Load the pretrained weights (if any)
+    ###############################
 
     if os.path.exists(config['train']['pretrained_weights']):
-        print "Loading pre-trained weights in", config['train']['pretrained_weights']
+        print ("Loading pre-trained weights in", config['train']['pretrained_weights'])
         yolo.load_weights(config['train']['pretrained_weights'])
 
     ###############################
-    #   Start the training process 
+    #   Start the training process
     ###############################
 
     yolo.train(train_imgs         = train_imgs,
                valid_imgs         = valid_imgs,
                train_times        = config['train']['train_times'],
                valid_times        = config['valid']['valid_times'],
-               nb_epoch           = config['train']['nb_epoch'], 
-               learning_rate      = config['train']['learning_rate'], 
+               nb_epoch           = config['train']['nb_epoch'],
+               learning_rate      = config['train']['learning_rate'],
                batch_size         = config['train']['batch_size'],
                warmup_epochs      = config['train']['warmup_epochs'],
                object_scale       = config['train']['object_scale'],
